e985d8b9cece5b8735f984936ec9c39d97705e51
yuntasha <jaljayo85@gmail.com>
2025-12-24 20:39:35 +0900

    [BOJ] 25311_UCPC에서 가장 쉬운 문제 번호는?

---
+++ yuntasha/4주차/25311.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        bf.readLine();
+
+        System.out.println('A');
+    }
+
+}
