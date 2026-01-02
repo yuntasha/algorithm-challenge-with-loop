cdb7416dcba3a117cba0cc89ccd7c959b37988f0
yuntasha <jaljayo85@gmail.com>
2026-01-02 20:54:38 +0900

    [BOJ] 34665_가희와 교통 요금

---
+++ yuntasha/5주차/34665.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(bf.readLine().equals(bf.readLine()) ? 0 : 1550);
+    }
+}
